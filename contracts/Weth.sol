c2d77ef4fbddfa01804555c03405c28353b98d0a
Md Mohsin Siddiqui <md@katchintech.com>
2021-08-23 17:20:30 +0530

    first commit

---
+++ contracts/Weth.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Weth {
+  function deposit() public payable {}
+  function approve(address spender, uint amount) external {}
+  function allowance(address owner, address spender) external view returns(uint) {}
+  function balanceOf(address owner) external view returns(uint) {}
+}
